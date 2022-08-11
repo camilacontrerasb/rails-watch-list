1356cef5dc535fb1f1dfe9f491a8f86ab2823bf6
Camila Contreras Barrera <camila1127@hotmail.ca>
2022-08-11 12:28:23 -0400

    finished models, created controllers (2) files, missing movies controllers?

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ApplicationRecord
+  validates :title, uniqueness: true, presence: true
+  validates :overview, uniqueness: true, presence: true
+  # validates :rating, numericality: { only_integer: true }
+  # validates :rating, presence: true
+  has_many :bookmarks
+  # has_many :movies
+  # has_many :movies, dependent: :destroy
 end
