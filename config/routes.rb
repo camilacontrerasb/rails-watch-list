44c1128268a362364797d7b448ce649b61f771b8
Camila Contreras Barrera <camila1127@hotmail.ca>
2022-08-12 12:08:01 -0400

    root added in routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # get 'lists/new'
   # get 'lists/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: "lists#index"
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
